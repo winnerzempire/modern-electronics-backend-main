d1686a2ccf3013e9a46927bc17bc2725de80361c
winnerzempire <hynicekwamboka@gmail.com>
2024-06-14 07:01:41 -0700

    Added Host

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o7ztw4ih^9mpj7&raimzf^cq!(mjinml@qa#s3mo21fp-8jrm2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
