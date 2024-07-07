44e560a7372cb0df5b7108f55d2e0c104343a860
Ubuntu <ubuntu@ip-172-31-20-15.ec2.internal>
2024-07-07 17:41:31 +0000

    Cloud File Changes

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o7ztw4ih^9mpj7&raimzf^cq!(mjinml@qa#s3mo21fp-8jrm2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
