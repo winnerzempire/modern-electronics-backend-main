0b1ad2187508422f515045b585f9f5473658b5bd
winnerzempire <hynicekwamboka@gmail.com>
2024-07-07 10:39:53 -0700

    Static Folder Change

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/"static/"
+STATIC_ROOT= os.path.join(BASE_DIR, "static")
 
 MEDIA_URL="media/"
-MEDIA_ROOT=BASE_DIR/"media/" 
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
