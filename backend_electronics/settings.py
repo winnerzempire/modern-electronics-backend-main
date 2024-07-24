f9c6666e0db03d41bcb74f633fd4e8e3c93a7af4
winnerzempire <hynicekwamboka@gmail.com>
2024-07-24 01:55:38 -0700

    Disable Debugging

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o7ztw4ih^9mpj7&raimzf^cq!(mjinml@qa#s3mo21fp-8jrm2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['0.0.0.0']
 ALLOWED_HOSTS = ['*']
