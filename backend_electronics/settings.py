ccd96ab3b77062608fd6d65af473afcac6cf1e97
winnerzempire <hynicekwamboka@gmail.com>
2024-09-12 05:13:52 -0700

    https edits

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR,  "static")
-MEDIA_URL="media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = 'https://https://viqtech.co.ke//static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (User-uploaded files)
+MEDIA_URL = 'https://https://viqtech.co.ke//media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
