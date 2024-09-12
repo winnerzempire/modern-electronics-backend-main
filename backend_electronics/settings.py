db8b12b63024784c5bbf37a93c27b27f92f7316b
winnerzempire <hynicekwamboka@gmail.com>
2024-09-12 04:44:17 -0700

    settings edits

--- backend_electronics/settings.py
+++ backend_electronics/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = True 
 
 
 ROOT_URLCONF = 'backend_electronics.urls'
