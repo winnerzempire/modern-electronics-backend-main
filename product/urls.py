a10f4373cf015ef132468931635f2a22acbb5d5d
winnerzempire <hynicekwamboka@gmail.com>
2024-09-14 03:07:50 -0700

    urls

--- product/urls.py
+++ product/urls.py
@@ -6,13 +6,14 @@ from .views import CategoryListCreateView, ProductListByCategory, category_list_
 
 
 urlpatterns = [
-    path("api/categories/", category_list_view, name="category-list"),
-    path("api/categories/class-based/", CategoryListCreateView.as_view(), name="category-list-cbv"),
-    path("api/product/categories/", category_list_view, name="user-category-list"), 
-    path("products/", product_list_view, name="product-list"),  # Lists all products
-    path("products/<int:pk>/", product_retrieve_view, name="product-detail"),  # Retrieves a single product by its primary key
-    path("reviews/create/", review_create_view, name="review-create"),  # Creates a new review
+    path("", CategoryListCreateView.as_view(), name='category-list-create'),
     path('products/category/selectedCategory/', ProductListByCategory.as_view(), name='products-by-category'),
+    path("create/", views.review_create_view, name="review-create"),
+    # path("api/categories/class-based/", CategoryListCreateView.as_view(), name="category-list-cbv"),
+    path("api/product/categories/", category_list_view, name="user-category-list"), 
+    path('products/', product_list_view, name='product-list'),  # Lists all products
+    path('products/<int:pk>/', product_retrieve_view, name='product-detail'),  # Retrieves a single product by its primary key
+    path('reviews/create/', review_create_view, name='review-create'),  # Creates a new review
     path('products/<int:category_id>/', ProductListByCategory.as_view(), name='product-list-by-category'),
-]
 
+]
